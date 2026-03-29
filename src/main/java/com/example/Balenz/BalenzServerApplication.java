b14a1b58a59db5bcf3552d8fa9001714d1b8a805
ruddmslee <dlruddms326@naver.com>
2026-03-30 04:41:08 +0900

    Chore: 초기 프로젝트 생성

---
+++ src/main/java/com/example/Balenz/BalenzServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.Balenz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalenzServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BalenzServerApplication.class, args);
+    }
+
+}
