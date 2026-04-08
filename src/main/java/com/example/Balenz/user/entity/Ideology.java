10d581e36ccbbb719051e6b92f816f392796d168
ruddmslee <dlruddms326@naver.com>
2026-04-09 01:38:36 +0900

    Feat: User 관련 엔티티, Enum 추가

---
+++ src/main/java/com/example/Balenz/user/entity/Ideology.java
@@ -0,0 +1,7 @@
+package com.example.Balenz.user.entity;
+
+public enum Ideology {
+    VALUE,   // 가치
+    NEUTRAL,
+    NORM     // 규범
+}
---
+++ src/main/java/com/example/Balenz/user/entity/User.java
@@ -0,0 +1,55 @@
+package com.example.Balenz.user.entity;
+
+import com.example.Balenz.global.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    // 소셜 로그인일 경우 null
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    private Ideology ideology;
+
+    private Integer yearOfBirth;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<RefreshToken> refreshTokens = new ArrayList<>();
+
+    @Builder
+    public User(String nickname, String email, String password, Role role) {
+        this.nickname = nickname;
+        this.email = email;
+        this.password = password;
+        this.role = (role == null) ? Role.ROLE_USER : role;
+    }
+
+}
