397acbd5cf155870b26eb7ba755dd9503f97efe5
ruddmslee <dlruddms326@naver.com>
2026-04-12 00:56:21 +0900

    Refactor: 재사용을 위해 쿠키 세팅 로직 메서드로 추출

--- src/main/java/com/example/Balenz/global/security/CookieUtil.java
+++ src/main/java/com/example/Balenz/global/security/CookieUtil.java
@@ -1,10 +1,25 @@
 package com.example.Balenz.global.security;
 
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CookieUtil {
+
+    @Value("${COOKIE_SAMESITE}")
+    private String COOKIE_SAMESITE;
+
+    @Value("${COOKIE_SECURE}")
+    private boolean COOKIE_SECURE;
+
+    /** accessToken, refreshToken 세팅 */
+    public void setTokenCookies(HttpServletResponse response, String accessToken, String refreshToken) {
+        addCookie(response, "accessToken", accessToken,
+                (int) (JwtProvider.EXPIRE_ACCESS / 1000), COOKIE_SECURE, COOKIE_SAMESITE);
+        addCookie(response, "refreshToken", refreshToken,
+                (int) (JwtProvider.EXPIRE_REFRESH / 1000), COOKIE_SECURE, COOKIE_SAMESITE);
+    }
 
     /** 쿠키 세팅 */
     public void addCookie(HttpServletResponse response,
