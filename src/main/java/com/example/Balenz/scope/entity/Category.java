2e4ed4c56cc182ccb6b1cca40288873cd5b68c0a
ruddmslee <dlruddms326@naver.com>
2026-04-20 18:55:24 +0900

    Feat: Scope 관련 Entity 추가

---
+++ src/main/java/com/example/Balenz/scope/entity/Category.java
@@ -0,0 +1,10 @@
+package com.example.Balenz.scope.entity;
+
+public enum Category {
+    POLITICS,
+    ECONOMY,
+    SOCIETY,
+    WORLD,
+    CULTURE,
+    TECHNOLOGY
+}
---
+++ src/main/java/com/example/Balenz/scope/entity/Scope.java
@@ -0,0 +1,39 @@
+package com.example.Balenz.scope.entity;
+
+import com.example.Balenz.article.entity.Article;
+import com.example.Balenz.global.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Scope extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String thumbnailUrl;
+
+    private String summary;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    @Column(nullable = false)
+    private Long viewCount = 0L;
+
+    @OneToMany(mappedBy = "scope")
+    private List<Article> articles = new ArrayList<>();
+
+}
